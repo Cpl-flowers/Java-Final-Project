6e394233880abae1dc193bf1c588b61a8559f4b1
Cpl-flowers <kevingabrielflores@hotmail.com>
2025-04-28 05:40:32 -0500

    V 0.4.0 Completed intial game screens added a death screen added victory screen added enemy goblin added 1 line of dialouge to the guard added new weapon added new item "silver ring"
    needs to include some level of
    static method, or attributes
    Encapsulation
    Inheritence
    Polymorphism
    Abstraction

--- Adventure_Game/src/App.java
+++ Adventure_Game/src/App.java
@@ -9,6 +9,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 
+import Assets.*;
+
 // Class called App
 public class App {
 
@@ -33,14 +35,14 @@ public class App {
 
     // allows for an area of text to display and sets its font attributes
     JTextArea mainTextArea;
-    Font bodyFont = new Font("Consolas", Font.PLAIN, 24);
-    Font headerFont = new Font("Consolas", Font.PLAIN, 24);
+    Font bodyFont = new Font("Consolas", Font.PLAIN, 20);
+    Font headerFont = new Font("Consolas", Font.PLAIN, 20);
 
     // creates the title screen handler
     TitleScreenHandler tsHandler = new TitleScreenHandler();
-    choiceHandler choiceHandler = new choiceHandler();
+    choiceHandler ChoiceHandler = new choiceHandler();
 
-    int playerHP;
+    int playerHP, monsterHP, silverRing;
     String playerWeapon, position;
 
     // main method
@@ -202,6 +204,7 @@ public class App {
     }
 
     // ChoiceButton allows instanciating buttons to be streamlined
+    // A setter method
     public JButton ChoiceButton(String x){
         JButton button;
         button = new JButton(x);
@@ -209,19 +212,30 @@ public class App {
         button.setForeground(Color.white);
         button.setFont(bodyFont);
         button.setFocusPainted(false);
-        button.addActionListener(choiceHandler);
+        button.addActionListener(ChoiceHandler);
         return button;
     }
 
+    // getter
     public void playerSetup() {
         playerHP = 15;
         playerWeapon = "Knife";
+
+        monsterHP = 20;
     
-        hpLabel.setText(("HP: " + playerHP));
+        hpLabel.setText("HP: " + playerHP);
         weaponLabel.setText("Weapon: " + playerWeapon);
 
         townGate();
 
+    }
+
+    // class that will hold scripts for interactive buttons that progress the game
+    public class TitleScreenHandler implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+
+            createGameScreen();
+        }
     }
     
     // unused code
@@ -240,8 +254,9 @@ public class App {
         }
     }
 
+    // getter
     public void townGate() {
-        position = "Town Gate";
+        position = "townGate";
         mainTextArea.setText("You are at the gate of the town. \nA guard is standing in front of you. \n\nWhat do you do?");
         choice1.setText("Talk to the guard.");
         choice2.setText("Attack the guard");
@@ -251,23 +266,259 @@ public class App {
     }
 
     public void talkGuard () {
-        position = "talkGuard";
-        mainTextArea.setText("Guard: Hello Stranger.\nI have never seen your face before.\nI'm sorry but I cannot let a stanger enter this town.");
-        choice1.setText(">");
+
+        if (silverRing < 1) {       
+            position = "talkGuard";
+            mainTextArea.setText("Guard: Hello Stranger.\nI have never seen your face before.\nI'm sorry but I cannot let a stanger enter this town.");
+            choice1.setText(">");
+            choice2.setText("");
+            choice3.setText("");
+            choice4.setText("");
+        }
+        else {
+            position = "endGame";
+            mainTextArea.setText("[You present your silver ring to the guard] \nGuard: Oh so you defeated that goblin, thank you!");
+            choice1.setText(">");
+            choice2.setText("");
+            choice3.setText("");
+            choice4.setText("");
+        }
     }
 
-        // class that will hold scripts for interactive buttons that progress the game
-        public class TitleScreenHandler implements ActionListener{
-            public void actionPerformed(ActionEvent event){
+    public void attackGuard() {
+        position = "attackGuard";
+        mainTextArea.setText("Guard: Hey! Don't act foolish! \n[The Guard strikes you with the butt of his sword] \n[You recieve 3 damage.]");
+        playerHP -= 3;
+        hpLabel.setText("HP: " + playerHP);
+        choice1.setText(">");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void crossRoad() {
+        position = "crossRoad";
+        mainTextArea.setText("[You leave town, you arrive at a cross roads. \nBehind you to the south is the town]");
+        choice1.setText("Go north");
+        choice2.setText("Go south");
+        choice3.setText("Go east");
+        choice4.setText("Go west");
+
+    }
+
+    public void north() {
+        position = "north";
+        mainTextArea.setText("[There is a river. You cup your hands to take a drink]\n[This heals you somewhat.]");
+        playerHP += 2;
+        hpLabel.setText("HP: " + playerHP);
+        choice1.setText("Return to road");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
     
-                createGameScreen();
-            }
+    public void east() {
+        position = "east";
+        mainTextArea.setText("[You walked into a forest]\n[You discovered a long sword]");
+        playerWeapon = "Long Sword";
+        weaponLabel.setText("Weapon: " + playerWeapon);
+        choice1.setText("Return to road");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+    
+    public void west() {
+        position = "west";
+        mainTextArea.setText("[You encountered a goblin!]");
+        if (monsterHP > 1) {   
+
+            choice1.setText("Fight");
+            choice2.setText("Run Away");
+            choice3.setText("");
+            choice4.setText("");
         }
 
-        public class choiceHandler implements ActionListener{
-            public void actionPerformed(ActionEvent event){
-    
-                String yourChoice = event.getActionCommand()
-            }
+        else {
+            mainTextArea.setText("nothing worth seeing here");
+            choice1.setText("Walk away");
+            choice2.setText("");
+            choice3.setText("");
+            choice4.setText("");
         }
+    }
+
+    public void fight() {
+        position = "fight";
+         
+        mainTextArea.setText("MonsterHP: " + monsterHP + "\n[What do you do?]");
+        choice1.setText("Swing your " + playerWeapon);
+        choice2.setText("Run Away");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void attackGoblin() {
+        position = "attackGoblin";
+
+        int playerDamage = 0;
+        int monsterdamage = 0;
+
+        if (playerWeapon.equals("Knife")) {
+            playerDamage = new java.util.Random().nextInt(4);
+        }
+        else if (playerWeapon.equals("Long Sword")) {
+            playerDamage = new java.util.Random().nextInt(9);
+        }
+
+        monsterHP -= playerDamage;
+        
+        // monsterdamage = new java.util.Random().nextInt(3);
+        // playerHP -= monsterdamage;
+        goblin.attack();
+        hpLabel.setText("HP: " + playerHP);
+
+        if (monsterHP < 1) {
+            position = "win";
+            mainTextArea.setText("[The goblin is defeated!]\n[A silver ring was dropped]");
+            silverRing = 1;
+            
+            choice1.setText("Walk away");
+            choice2.setText("");
+            choice3.setText("");
+            choice4.setText("");
+        }
+        else if (playerHP < 1){
+            position = "playerDeath";
+            mainTextArea.setText("[You were slain]");
+
+            choice1.setText("Death arrives");
+            choice2.setText("");
+            choice3.setText("");
+            choice4.setText("");
+
+        }
+        else {
+            mainTextArea.setText("[You swing your " + playerWeapon +  " at the goblin]\n[you dealt " + playerDamage
+            + " damage to the goblin]\n[The goblin now has " + monsterHP + " health]"
+            + "\n[The goblin swings back with its claw for " + monsterdamage + " damage]");
+
+            choice1.setText("Swing again");
+            choice2.setText("Run away");
+            choice3.setText("");
+            choice4.setText("");
+        }
+    }
+
+    public void playerDeath() {
+        position = "playerDeath";
+
+        window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
+    }
+
+    public void endGame() {
+        mainTextArea.setText("[Congradulations you have beaten the game.\nThank you so much for playing]");
+    }
+
+
+    // This handles the choice buttons so that when the user clicks on a button the windown listens for the action an registers a response
+    public class choiceHandler implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+
+            String yourChoice = event.getActionCommand();
+
+            switch (position) {
+            case "townGate":
+                switch(yourChoice) {
+                case "c1": talkGuard(); break;
+                case "c2": attackGuard(); break;
+                case "c3": crossRoad(); break;
+                case "c4": break; 
+                }
+            break;
+
+            case "talkGuard":
+                switch (yourChoice) {
+                case "c1": townGate(); break;
+                }
+            break;
+
+            case "attackGuard":
+                switch (yourChoice) {
+                case "c1": townGate(); break;
+                }
+            break;
+
+            case "crossRoad":
+                switch (yourChoice) {
+                case "c1": north(); break; 
+                case "c2": townGate(); break;
+                case "c3": east(); break;
+                case "c4": west(); break;
+                }
+            break;
+
+            case "north":
+                switch (yourChoice) {
+                    case "c1": crossRoad(); break;
+                }
+            break;
+            
+            case "east":
+                switch (yourChoice) {
+                    case "c1": crossRoad(); break;
+                }
+            break;
+            
+            case "west":
+                if (monsterHP < 1) {
+                switch (yourChoice) {
+                    case "c1": crossRoad(); break;
+                    case "c2": crossRoad(); break;
+                    }
+                }
+                else {
+                switch (yourChoice) {
+                    case "c1": fight(); break;
+                    case "c2": crossRoad(); break;
+                    }
+                }
+            
+            break;
+
+            case "fight":
+                switch (yourChoice) {
+                    case "c1": attackGoblin(); break;
+                    case "c2": crossRoad(); break;
+                }
+            break;
+
+            case "attackGoblin":
+                switch (yourChoice) {
+                    case "c1": attackGoblin(); break;
+                    case "c2": crossRoad(); break;
+                }
+            break;
+
+            case "playerDeath":
+                switch (yourChoice) {
+                    case "c1": playerDeath(); break;
+                }
+            break;
+
+            case "win":
+                switch (yourChoice) {
+                    case "c1": crossRoad();
+                }
+            break;
+
+            case "endGame": 
+                switch (yourChoice) {
+                    case "c1": endGame(); break;
+            }
+            break;
+        }
+    }
+}
 }
